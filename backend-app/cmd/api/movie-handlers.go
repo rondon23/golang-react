f5ea4746dac5066bd8fce76c8b64251c1581fcfe
rondon23 <brunorondon21@gmail.com>
2022-03-01 00:23:55 -0400

    getting all movies as JSON

--- backend-app/cmd/api/movie-handlers.go
+++ backend-app/cmd/api/movie-handlers.go
@@ -19,26 +19,41 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.logger.Println("ïd is", id)
-
 	movie, err := app.models.DB.Get(id)
 
-	// movie := models.Movie{
-	// 	ID:          id,
-	// 	Title:       "Some movie",
-	// 	Description: "Some description",
-	// 	Year:        2022,
-	// 	ReleaseDate: time.Date(2022, 01, 01, 01, 0, 0, 0, time.Local),
-	// 	Runtime:     100,
-	// 	Rating:      5,
-	// 	MPAARating:  "PG-13",
-	// 	CreatedAt:   time.Now(),
-	// 	UpdatedAt:   time.Now(),
-	// }
-
 	err = app.writeJSON(w, http.StatusOK, movie, "movie")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 }
 
 func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
+	movies, err := app.models.DB.All()
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movies, "movies")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+}
+
+func (app *application) deleteMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) insertMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) updateMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) searchMovie(w http.ResponseWriter, r *http.Request) {
 
 }
