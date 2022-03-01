4b157bd0df638bc26ea470a759b95e568c8fa01d
rondon23 <brunorondon21@gmail.com>
2022-03-01 14:20:49 -0400

    checking for errors

--- go-movies/src/components/Movies.js
+++ go-movies/src/components/Movies.js
@@ -1,28 +1,45 @@
 import React, { Component, Fragment } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export default class Movies extends Component {
     state = {
         movies: [],
         isLoaded: false,
+        error: null,
     };
 
     componentDidMount() {
         fetch("http://localhost:4000/v1/movies")
-            .then((response) => response.json())
+            .then((response) => {
+
+                if (response.status !== "200") {
+                    let err = Error;
+                    err.message = "Invalid response code: " + response.status;
+                    this.setState({ error: err });
+                }
+                return response.json();
+            })
             .then((json) => {
                 this.setState({
                     movies: json.movies,
                     isLoaded: true,
-                })
+                },
+                    (error) => {
+                        this.setState({
+                            isLoaded: true,
+                            error
+                        });
+                    }
+                );
             })
     }
 
     render() {
-        const { movies, isLoaded } = this.state;
-
-        if (!isLoaded) {
-            return <p>Loading</p>
+        const { movies, isLoaded, error } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>
+        } else if (!isLoaded) {
+            return <p>Loading...</p>
         } else {
             return (
                 <Fragment>
