fcb1881ccf0cbcc5b8657b1b982c31ebb5cba28d
rondon23 <brunorondon21@gmail.com>
2022-03-08 00:13:49 -0400

    getting movies by genre.

--- go-movies/src/App.js
+++ go-movies/src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch } from 'rea
 import Movies from "./components/Movies";
 import Admin from "./components/Admin";
 import Home from "./components/Home";
-import Categories from "./components/Categories";
 import OneMovie from "./components/OneMovie";
+import Genres from "./components/Genres";
 
 export default function App() {
   return (
@@ -29,7 +29,7 @@ export default function App() {
                 </li>
 
                 <li className="list-group-item">
-                  <Link to="/by-category">Categories</Link>
+                  <Link to="/genres">Genres</Link>
                 </li>
 
                 <li className="list-group-item">
@@ -42,24 +42,14 @@ export default function App() {
           <div className="col-md-10">
             <Switch>
 
-              <Route path="/movies/:id" component={OneMovie}/>
+              <Route path="/movies/:id" component={OneMovie} />
 
               <Route path="/movies">
                 <Movies />
               </Route>
-              <Route exact path="/by-category">
-                <CategoryPage />
+              <Route exact path="/genres">
+                <Genres />
               </Route>
-
-              <Route
-                exact
-                path="/by-category/drama"
-                render={(props) => <Categories {...props} title={`Drama`} />} />
-
-              <Route
-                exact
-                path="/by-category/comedy"
-                render={(props) => <Categories {...props} title={`Comedy`} />} />
 
               <Route path="/admin">
                 <Admin />
@@ -73,20 +63,5 @@ export default function App() {
         </div>
       </div>
     </Router>
-  );
-}
-
-function CategoryPage() {
-  let { path, url } = useRouteMatch();
-
-  return (
-    <div>
-      <h2>Categories</h2>
-
-      <ul>
-        <li> <Link to={`${path}/comedy`}>Comedy</Link> </li>
-        <li> <Link to={`${path}/drama`}>Drama</Link> </li>
-      </ul>
-    </div>
   );
 }
