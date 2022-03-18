87849b357e30839eb9f8f2afe7b20b5c1f5ada9a
rondon23 <brunorondon21@gmail.com>
2022-03-17 21:29:15 -0400

    connecting delete button to the rest backend

--- backend-app/cmd/api/routes.go
+++ backend-app/cmd/api/routes.go
@@ -15,7 +15,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:genre_id", app.getAllMoviesByGenre)
 	router.HandlerFunc(http.MethodGet, "/v1/genres", app.getAllGenres)
 	router.HandlerFunc(http.MethodPost, "/v1/admin/editmovie", app.editmovie)
-	router.HandlerFunc(http.MethodDelete, "/v1/admin/deletemovie/:id", app.deleteMovie)
+	router.HandlerFunc(http.MethodGet, "/v1/admin/deletemovie/:id", app.deleteMovie)
 
 	return app.enableCORS(router)
 }
--- go-movies/src/components/EditMovie.js
+++ go-movies/src/components/EditMovie.js
@@ -141,15 +141,27 @@ export default class EditMovie extends Component {
     }
 
     confirmDelete = (e) => {
-        console.log("would delete movie id", this.state.movie.id)
-
         confirmAlert({
             title: 'Delete Movie?',
             message: 'Are you sure?',
             buttons: [
                 {
                     label: 'Yes',
-                    onClick: () => alert('Click Yes')
+                    onClick: () => {
+                        fetch("http://localhost:4000/v1/admin/deletemovie/" + this.state.movie.id, { method: "GET" })
+                            .then(response => response.json)
+                            .then(data => {
+                                if (data.error) {
+                                    this.setState({
+                                        alert: { type: "alert-danger", message: data.error.message }
+                                    })
+                                } else {
+                                    this.props.history.push({
+                                        pathname: "/admin",
+                                    })
+                                }
+                            });
+                    }
                 },
                 {
                     label: 'No',
