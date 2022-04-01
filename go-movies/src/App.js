b24456bc8293dcb01a858a1a86148225241b4b74
rondon23 <brunorondon21@gmail.com>
2022-04-01 12:21:23 -0400

    implementing GraphQl requests for all movies

--- go-movies/src/App.js
+++ go-movies/src/App.js
@@ -8,6 +8,7 @@ import Genres from "./components/Genres";
 import OneGenre from "./components/OneGenre";
 import EditMovie from "./components/EditMovie";
 import Login from "./components/Login";
+import GraphQL from "./components/GraphQL";
 
 export default class App extends Component {
 
@@ -83,6 +84,9 @@ export default class App extends Component {
                       </li>
                     </Fragment>
                   )}
+                  <li className="list-group-item">
+                    <Link to="/graphql">GraphQL</Link>
+                  </li>
                 </ul>
 
               </nav>
@@ -111,6 +115,10 @@ export default class App extends Component {
 
                 <Route exact path="/genres">
                   <Genres />
+                </Route>
+
+                <Route exact path="/graphql">
+                  <GraphQL/>
                 </Route>
 
                 <Route
